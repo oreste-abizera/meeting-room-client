07d482a68d19aba0fea95c4d18e61dd386376b74
oreste-abizera <oresteabizera11@gmail.com>
2023-01-09 16:48:28 +0200

    chore: toast library condiguration

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,30 @@ import "./index.css";
 import "./static/css/index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+      gutter={8}
+      containerClassName=""
+      containerStyle={{}}
+      toastOptions={{
+        // Define default options
+        className: "",
+        duration: 5000,
+        style: {
+          background: "#363636",
+          color: "#fff",
+        },
+        // Default options for specific types
+        success: {
+          duration: 3000,
+        },
+      }}
+    />
     <Router>
       <App />
     </Router>
