e21dae1f8b3183e18d365580d2fdee3cf18a0303
oreste-abizera <oresteabizera11@gmail.com>
2023-01-17 00:16:15 +0200

    feat: layout responsiveness

---
+++ src/components/reusable/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Sidebar = (props: Props) => {
+  return (
+    <div className="w-[70%] lg:w-full h-full overflow-y-auto bg-[#363740]">
+      Sidebar
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,11 +1,36 @@
 import React from "react";
+import Sidebar from "../components/reusable/Sidebar";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const DashboardLayout = ({ children }: Props) => {
-  return <div>DashboardLayout</div>;
+  const [isOpen, setIsOpen] = React.useState(true);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="w-full h-[100vh] flex justify-end">
+      <div
+        className={`animated-sideview w-full lg:w-[17.7%] h-full fixed top-0 ${
+          isOpen ? "left-0" : "-left-[100%]"
+        } lg:left-0`}
+      >
+        <div
+          className="absolute top-0 right-0 w-[20%] h-full"
+          onClick={handleToggle}
+        ></div>
+        <Sidebar />
+      </div>
+      <div
+        className="w-full lg:w-[82.3%] h-full bg-gray-500"
+        onClick={handleToggle}
+      ></div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
