39ab4fcbb68c036a7ffab8c307d2e9f2f3a39c04
oreste-abizera <oresteabizera11@gmail.com>
2023-01-19 03:24:39 +0200

    chore: types

--- src/types/index.ts
+++ src/types/index.ts
@@ -37,3 +37,48 @@ export type UserDTO = {
   updatedAt: String;
   __v: Number;
 };
+
+export type Building = {
+  _id: String;
+  name: String;
+  address: String;
+  floors: Number;
+  image: {
+    image_url: String;
+    public_id: String;
+  };
+  description?: String;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: Number;
+};
+
+export type Place = {
+  _id: String;
+  name: String;
+  location: String;
+  floor: Number;
+  building: Building;
+  images: [
+    {
+      image_url: String;
+      public_id: String;
+    }
+  ];
+  description?: String;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: Number;
+};
+
+export type Booking = {
+  _id: String;
+  place: Place;
+  user: UserDTO;
+  startTime: Date;
+  endTime: Date;
+  status: String;
+  createdAt: Date;
+  updatedAt: Date;
+  __v: Number;
+};
